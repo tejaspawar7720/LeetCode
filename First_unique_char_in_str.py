952e76fd4679c7dee5125ebb1e24e90489ba0ae7
unknown <Tejas Pawar>
2026-04-29 12:43:54 +0530

    First unique char in string

---
+++ First_unique_char_in_str.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
+
+        return -1
